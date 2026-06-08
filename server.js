e3a06f195731060f69ef6b882d7985587f7e2367
Hafidz <hafidzsevenfoldism@gmail.com>
2026-06-09 06:05:14 +0700

    fix: tambah origin GitHub Pages ke CORS whitelist + update .env.example

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ const allowedOrigins = [
   'http://127.0.0.1:5501',
   'http://localhost:5501',
   'http://localhost:3000',
+  'https://hafidzsevenfoldism-rgb.github.io',
 ];
 
 if (process.env.FRONTEND_URL) {
