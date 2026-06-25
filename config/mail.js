cf3d32cccea3564effb55f8457a44c899fe9778e
Hafidz <hafidzsevenfoldism@gmail.com>
2026-06-26 05:37:33 +0700

    fix: FRONTEND_URL tanpa /frontend

--- config/mail.js
+++ config/mail.js
@@ -11,7 +11,7 @@ const transporter = nodemailer.createTransport({
 });
 
 async function kirimEmailReset(to, token) {
-  const resetLink = (process.env.FRONTEND_URL || 'http://localhost:5500/frontend') +
+  const resetLink = (process.env.FRONTEND_URL || 'http://localhost:5500') +
     '/resetpassword.html?token=' + token;
 
   await transporter.sendMail({
